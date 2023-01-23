56c61b3f51ba9c641028c6e7cae5b7f45eb491ba
Kleyton <kleyton_vccamargo@hotmail.com>
2023-01-22 22:15:06 -0300

    first commit

---
+++ src/main/java/com/ola/demo/controller/OlaController.java
@@ -0,0 +1,15 @@
+package com.ola.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ola")
+public class OlaController {
+    
+    @GetMapping
+    public String getOla() {
+        return "Olá Spring";
+    }
+}
